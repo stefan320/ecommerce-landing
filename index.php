c023fcfb544145f989859b740cc9322a67f923cd
stefan320 <stefan_014_@hotmail.com>
2019-09-29 20:43:44 +0200

    Completed mobile version

--- index.php
+++ index.php
@@ -10,113 +10,168 @@
 	<title>wp_title();</title>
 </head>
 <body>
+	<!-- Header -->
 	<header class="header">
-	<img alt="navigation" class="nav--btn" src="<?php echo get_theme_file_uri('assets/icons/menu.svg'); ?>">
-		<nav class="nav">
-			<a class="nav--link" href="#">Sale</a>
-			<a class="nav--link" href="#">New In</a>
-			<a class="nav--link" href="#">Store Locator</a>
-		</nav>
+		<!-- NAV -->
+		<div class="nav">
+		<input type="checkbox" class="nav--checkbox" id="nav--checkbox">
+		<label for="nav--checkbox" class="nav--label">
+			<img alt="navigation button" class="nav--btn" src="<?php echo get_theme_file_uri('assets/icons/menu.svg'); ?>">
+		</label>
+			<nav class="nav__list">
+				<a class="nav__list--link" href="#sale">Sale</a>
+				<a class="nav__list--link" href="#new--in">New In</a>
+				<a class="nav__list--link" href="#store--locator">Store Locator</a>
+			</nav>
+		</div>
+		<!-- /NAV -->
 		<h1 class="header--logo">SHOP LOGO</h1>
 		<img alt="Shopping Cart" class="header--cart" src="<?php echo get_theme_file_uri('assets/icons/shopping-bag.svg'); ?>">
 	</header>
+	<!-- /Header -->
 	<section class="promo">
 		<div class="promo__box">
-			<h3 class="promo__box--heading">15&amp; Off &plus; Free US Delivery</h3>
-			<h6 class="promo__box--sub">Subscribe To Our Newsletter</h6>
+			<h2 class="promo--heading heading__secondary text--white">15&amp; Off &plus; Free US Delivery</h2>
+			<p class="promo--sub heading__secondary--sub">Subscribe To Our Newsletter</p>
 		</div>
 		<div class="promo__box promo__box--main">
-			<h3 class="promo__box--heading">Free Standard Delivery</h3>
-			<h6 class="promo__box--sub">Over &dollar;25</h6>
+			<h2 class="promo--heading heading__secondary text--white">Free Standard Delivery</h2>
+			<p class="promo--sub heading__secondary--sub">Over &dollar;15</p>
 		</div>
 		<div class="promo__box">
-			<h3 class="promo__box--heading">20&amp; Off Discount</h3>
-			<h6 class="promo__box--sub">For Students</h6>
+			<h2 class="promo--heading heading__secondary text--white">20&amp; Off Discount</h2>
+			<p class="promo--sub heading__secondary--sub">For Students</p>
 		</div>
 	</section>
  
 	<!-- Slider -->
-	<section class="banner">
+	<section class="banner" id="sale">
 		<div class="banner__slider">
-		<div><img alt="UP TO 70% SALE!" src="<?php echo get_theme_file_uri('assets/images/Bitmap.png'); ?>" ></div>
-		<div><img alt="UP TO 70% SALE!" src="<?php echo get_theme_file_uri('assets/images/Bitmap.png'); ?>" ></div>
-		<div><img alt="UP TO 70% SALE!" src="<?php echo get_theme_file_uri('assets/images/Bitmap.png'); ?>" ></div>
+		<div>
+			<picture>
+				<source srcset="<?php echo get_theme_file_uri('assets/images/Bitmap--small.png'); ?>" media="(max-width: 800px)">
+				<img alt="UP TO 70% SALE!" class="slick--img" src="<?php echo get_theme_file_uri('assets/images/Bitmap.png'); ?>" />
+			</picture>
+		</div>
+		<div>
+			<picture>
+				<source srcset="<?php echo get_theme_file_uri('assets/images/Bitmap--small.png'); ?>" media="(max-width: 800px)">
+				<img alt="UP TO 70% SALE!" class="slick--img" src="<?php echo get_theme_file_uri('assets/images/Bitmap.png'); ?>" />
+			</picture>
+		</div>
+		<div>
+			<picture>
+				<source srcset="<?php echo get_theme_file_uri('assets/images/Bitmap--small.png'); ?>" media="(max-width: 800px)">
+				<img alt="UP TO 70% SALE!" class="slick--img" src="<?php echo get_theme_file_uri('assets/images/Bitmap.png'); ?>" />
+			</picture>
+		</div>
+		<div>
+			<picture>
+				<source srcset="<?php echo get_theme_file_uri('assets/images/Bitmap--small.png'); ?>" media="(max-width: 800px)">
+				<img alt="UP TO 70% SALE!" class="slick--img" src="<?php echo get_theme_file_uri('assets/images/Bitmap.png'); ?>" />
+			</picture>
+		</div>
 		</div>
 	</section>
+	<!-- /SLIDER -->
 
-	<section class="products">
-		<h3 class="heading__secondary">NEW IN WOMAEN'S FASHION</h3>
-		<p class="heading__tertiary--sub">Shop a fresh start to the season and choose your look from our new collection. From the best sellers to brand new arrivals and current seasonal trends, we have all occasions covered. We'll keep your style up to date, all you have to do is shop.</p>
+	<!-- Products -->
+	<section class="products" id="new--in">
+		<h1 class="heading__primary  products--title">NEW IN WOMEN'S FASHION</h1>
+		<p class="heading__secondary--sub products--sub text--light">Shop a fresh start to the season and choose your look from our new collection. From the best sellers to brand new arrivals and current seasonal trends, we have all occasions covered. We'll keep your style up to date, all you have to do is shop.</p>
 		<ul class="products__list">
 			<li class="products__list--item">
 				<div class="products__list--div">
-					<div class="products__list--img"></div>
-					<h2 class="heading__secondary">Dress</h2>
-					<p class="heading__secondary--sub">Dress with a tropical print palms</p>
-					<div class="products__list--price">&dollar;&nbsp;42</div>
+					<img alt="UP TO 70% SALE!" class="products__list--img" src="<?php echo get_theme_file_uri('assets/images/item-3.png'); ?>" >
+						<div class="products__list--text">				
+							<h2 class="heading__secondary">Dress</h2>
+							<p class="heading__secondary--sub text--light">Dress with a tropical print palms bardot style.</p>
+						</div>
+					<p class="products__list--price text--white">&dollar;&nbsp;42</p>
 				</div>
 			</li>
+
 			<li class="products__list--item">
 				<div class="products__list--div">
-					<div class="products__list--img"></div>
-					<h2 class="heading__secondary">Dress</h2>
-					<p class="heading__secondary--sub">Dress with a tropical print palms</p>
-					<div class="products__list--price">&dollar;&nbsp;42</div>
+					<img alt="UP TO 70% SALE!" class="products__list--img" src="<?php echo get_theme_file_uri('assets/images/item-3.png'); ?>" >
+						<div class="products__list--text">				
+							<h2 class="heading__secondary">Dress</h2>
+							<p class="heading__secondary--sub text--light">Dress with a tropical print palms bardot style.</p>
+						</div>
+					<p class="products__list--price text--white">&dollar;&nbsp;42</p>
 				</div>
 			</li>
+
 			<li class="products__list--item">
 				<div class="products__list--div">
-					<div class="products__list--img"></div>
-					<h2 class="heading__secondary">Dress</h2>
-					<p class="heading__secondary--sub">Dress with a tropical print palms</p>
-					<div class="products__list--price">&dollar;&nbsp;42</div>
+					<img alt="UP TO 70% SALE!" class="products__list--img" src="<?php echo get_theme_file_uri('assets/images/item-3.png'); ?>" >
+						<div class="products__list--text">				
+							<h2 class="heading__secondary">Dress</h2>
+							<p class="heading__secondary--sub text--light">Dress with a tropical print palms bardot style.</p>
+						</div>
+					<p class="products__list--price text--white">&dollar;&nbsp;42</p>
 				</div>
 			</li>
 		</ul>
-		<button href="#" class="btn">SHOP NOW</button>
+		<button href="#" class="btn btn--danger">SHOP NOW</button>
 	</section>
+	<!-- Products -->
 
+	<!-- Contact -->
 	<section class="contact">
-		<h3 class="heading__tertiary">CONTACT US</h3>
-		<form class="contact__form" action="#">
-			<input type="email" class="contact__form--email">
-			<textarea name="Message" id="contact--msg" class="contact__form--msg"></textarea>
-			<button type="submit" class="btn">CONTACT</button>
-		</form>
-		<div class="contact__locator">
-		<div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Halmann%20Vella%20Building%2C%20Mosta%20Road%2C%20Lija%2C%20%C4%A6al%20Lija%20LJA%209061&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>Google Maps Generator by <a href="https://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:100%;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:100%;}</style></div>
-		</div>
-	</section>
-
-	<footer class="footer">
-		<div class="footer__info">
-			<h3 class="heading__tertiary footer__info--header">ABOUT</h3>
-			<ul class="footer__info--ul">
-				<li class="footer__info--li"><a href="#" class="footer__info--link">Why Us?</a></li>
-				<li class="footer__info--li"><a href="#" class="footer__info--link">Store Locator</a></li>
-				<li class="footer__info--li"><a href="#" class="footer__info--link">Contact Us</a></li>
-			</ul>
-			<h3 class="heading__tertiary footer__info--header">DELIVERY</h3>
-			<ul class="footer__info--ul">
-				<li class="footer__info--li"><a href="#" class="footer__info--link">Delivery Info</a></li>
-				<li class="footer__info--li"><a href="#" class="footer__info--link">International Delivery</a></li>
-				<li class="footer__info--li"><a href="#" class="footer__info--link">Student Discount</a></li>
-			</ul>
-			<h3 class="heading__tertiary footer__info--header">HELP</h3>
-			<ul class="footer__info--ul">
-				<li class="footer__info--li"><a href="#" class="footer__info--link">Track My Order</a></li>
-				<li class="footer__info--li"><a href="#" class="footer__info--link">Returns & Refunds</a></li>
-				<li class="footer__info--li"><a href="#" class="footer__info--link">Payments Options</a></li>
-			</ul>
-		</div>
-		<div class="footer__newsletter">
-			<h3 class="header__tertiary">NEWSLETTER</h3>
-			<p class="header__tertiary--sub">Want to receive exclusive offers?</p>
-			<form action="#" class="footer__newsletter--form">
-				<input type="email" name="newsletter--email" id="newsletter--email">
-				<button type="submit">SUBSCRIBE</button>
+		<div class="contact__form">
+			<h2 class="heading__secondary contact--title">CONTACT US</h2>
+			<form id="contact__form" action="#">
+				<input type="email" class="contact__form--input" placeholder="Your email">
+				<textarea name="Message" id="contact--msg" class="contact__form--msg" placeholder="Message"></textarea>
+				<button type="submit" class="btn btn--dark btn--full">CONTACT</button>
 			</form>
 		</div>
+		<div class="contact__locator" id="store--locator">
+		<h2 class="heading__secondary contact--title">STORE LOCATOR</h2>
+
+		<!-- MAP -->
+		<div class="mapouter contact--map"><div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Halmann%20Vella%20Building%2C%20Mosta%20Road%2C%20Lija%2C%20%C4%A6al%20Lija%20LJA%209061&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>Google Maps Generator by <a href="https://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:100%;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:100%;}</style></div>
+		</div>
+		<!-- /MAP -->
+
+	</section>
+	<!-- /Contact -->
+
+	<footer class="footer">
+		<!-- Footer Info -->
+		<div class="footer__info">
+			<h2 class="heading__secondary footer__info--header">ABOUT</h2>
+				<ul class="footer__info--ul">
+					<li class="footer__info--li"><a href="#" class="footer__info--link">Why Us?</a></li>
+					<li class="footer__info--li"><a href="#" class="footer__info--link">Store Locator</a></li>
+					<li class="footer__info--li"><a href="#" class="footer__info--link">Contact Us</a></li>
+				</ul>
+				<h2 class="heading__secondary footer__info--header">DELIVERY</h2>
+				<ul class="footer__info--ul">
+					<li class="footer__info--li"><a href="#" class="footer__info--link">Delivery Info</a></li>
+					<li class="footer__info--li"><a href="#" class="footer__info--link">International Delivery</a></li>
+					<li class="footer__info--li"><a href="#" class="footer__info--link">Student Discount</a></li>
+				</ul>
+				<h2 class="heading__secondary footer__info--header">HELP</h2>
+				<ul class="footer__info--ul">
+					<li class="footer__info--li"><a href="#" class="footer__info--link">Track My Order</a></li>
+					<li class="footer__info--li"><a href="#" class="footer__info--link">Returns & Refunds</a></li>
+					<li class="footer__info--li"><a href="#" class="footer__info--link">Payments Options</a></li>
+				</ul>
+		</div>
+		<!-- /Footer Info -->
+
+		<!-- Footer Newsletter -->
+		<div class="footer__newsletter">
+			<h2 class="header__secondary footer__newsletter--header">NEWSLETTER</h2>
+			<p class="heading__secondary--sub footer__newsletter--sub">Want to receive exclusive offers?</p>
+			<form action="#" class="footer__newsletter--form clearfix">
+				<input type="email" class="footer__newsletter--email" name="newsletter--email" id="newsletter--email" placeholder="Enter your email address">
+				<button type="submit" class="footer__newsletter--btn">SUBSCRIBE</button>
+			</form>
+		</div>
+		<!-- /Footer Newsletter -->
 	</footer>
 
 	<?php wp_footer(); ?>
--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   $(".banner__slider").slick({
+    appendArrows: $(".banner__slider"),
     // prev arrow
     prevArrow:
-      '<button type="button" data-role="none" class="slick-prev"></button>',
+      '<button type="button" data-role="none" class="slick__arrow slick__arrow--prev"></button>',
     // next arrow
     nextArrow:
-      '<button type="button" data-role="none" class="slick-next"></button>',
-    autoplay: true,
+      '<button type="button" data-role="none" class="slick__arrow slick__arrow--next"></button>',
+    autoplay: false,
     // Auto play change interval
     autoplaySpeed: 2000,
     // Transition speed
